47ab2081dac7ee7cf7002fd0221bada6ae2685b7
Erick Otenyo <otenyo.erick@gmail.com>
2018-01-13 16:17:27 +0300

    add redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { APP_LOAD } from "../constants/actionTypes";
+import Header from "./Header";
 import Home from "./Home";
-
+import Mapp from "./Map";
+import { connect } from "react-redux";
 import "./App.css";
+const mapStateToProps = state => {
+  return {
+    appLoaded: state.common.appLoaded,
+    appName: state.common.appName
+  };
+};
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
-    </Switch>
-  </Router>
-);
+const mapDispatchToProps = dispatch => ({
+  onLoad: (payload, token) => dispatch({ type: APP_LOAD, token })
+});
 
-export default App;
+class App extends Component {
+  componentWillMount() {
+    const token = window.localStorage.getItem("jwt");
+    this.props.onLoad(token ? { user: true } : null, token);
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+            <Route path={`${process.env.PUBLIC_URL}/map`} component={Mapp} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
