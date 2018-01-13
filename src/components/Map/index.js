9e6c2ed50e2f18e452d85f3025cd5620dab86ecb
Erick Otenyo <otenyo.erick@gmail.com>
2018-01-14 01:15:36 +0300

    refactor layer

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,26 +2,13 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import injectTapEventPlugin from "react-tap-event-plugin";
-import { Jane, JaneLayer, MapLayer, Source } from "jane-maps";
+import { Jane } from "jane-maps";
 import { mapboxGLOptions } from "../../config";
+import Destinations from "./Destinations";
 
 import "jane-maps/dist/styles.css";
 
 injectTapEventPlugin();
-
-const featureSource = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: {},
-      geometry: {
-        type: "Point",
-        coordinates: [36.8219, -1.2921]
-      }
-    }
-  ]
-};
 
 class Mapp extends Component {
   render() {
@@ -30,26 +17,7 @@ class Mapp extends Component {
         <MuiThemeProvider>
           <div className="fullscreen">
             <Jane mapboxGLOptions={mapboxGLOptions}>
-              <JaneLayer
-                id="feature"
-                name="Feature"
-                icon="university"
-                defaultSelected
-                component={<div> This is a simple feature </div>}
-              >
-                <Source id="feature" type="geojson" data={featureSource} />
-
-                <MapLayer
-                  id="feature"
-                  source="feature"
-                  type="circle"
-                  paint={{
-                    "circle-radius": 10,
-                    "circle-color": "steelblue",
-                    "circle-opacity": 0.7
-                  }}
-                />
-              </JaneLayer>
+              <Destinations />
             </Jane>
           </div>
         </MuiThemeProvider>
