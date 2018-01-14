ab8574699681b9e4c1b217c22ac177480810a5da
Erick Otenyo <otenyo.erick@gmail.com>
2018-01-14 19:02:34 +0300

    add magical

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,11 @@
 import home from "./reducers/home";
 import common from "./reducers/common";
+import magical from "./reducers/magical";
 
 import { combineReducers } from "redux";
 
 export default combineReducers({
   home,
-  common
+  common,
+  magical
 });
