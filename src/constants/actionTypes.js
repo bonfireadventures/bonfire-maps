59edd323848cd2628de3d1db4f83cc30c14f5dc0
Erick Otenyo <otenyo.erick@gmail.com>
2018-01-14 19:01:59 +0300

    add

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,6 @@
 export const APP_LOAD = "APP_LOADED";
 export const HOME_PAGE_LOADED = "HOME_PAGE_LOADED";
 export const HOME_PAGE_UNLOADED = "HOME_PAGE_UNLOADED";
-export const ASYNC_START = 'ASYNC_START';
-export const ASYNC_END = 'ASYNC_END';
+export const ASYNC_START = "ASYNC_START";
+export const ASYNC_END = "ASYNC_END";
+export const MAP_PAGE_LOADED = "MAP_PAGE_LOADED";
