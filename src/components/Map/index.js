ab9e6effca269bdfd6b5ac9f35f2cf2f76d57323
Erick Otenyo <otenyo.erick@gmail.com>
2018-01-14 19:02:17 +0300

    connect redux

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { MAP_PAGE_LOADED } from "../../constants/actionTypes";
+import agent from "../../agent";
 import { connect } from "react-redux";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import injectTapEventPlugin from "react-tap-event-plugin";
@@ -10,7 +12,16 @@ import "jane-maps/dist/styles.css";
 
 injectTapEventPlugin();
 
+const mapDispatchToProps = dispatch => ({
+  onLoad: payload => dispatch({ type: MAP_PAGE_LOADED, payload })
+});
+
 class Mapp extends Component {
+  componentWillMount() {
+    const sitesPromise = agent.Data.magical;
+    this.props.onLoad(sitesPromise());
+  }
+
   render() {
     return (
       <div>
@@ -26,4 +37,4 @@ class Mapp extends Component {
   }
 }
 
-export default connect(null)(Mapp);
+export default connect(null, mapDispatchToProps)(Mapp);
