09d33aebbbaf02f34aa578ee31c8e3f57d4f6cab
Erick Otenyo <otenyo.erick@gmail.com>
2018-01-18 22:58:08 +0300

    add DEST CLICK

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -4,3 +4,4 @@ export const HOME_PAGE_UNLOADED = "HOME_PAGE_UNLOADED";
 export const ASYNC_START = "ASYNC_START";
 export const ASYNC_END = "ASYNC_END";
 export const MAP_PAGE_LOADED = "MAP_PAGE_LOADED";
+export const DEST_CLICKED = "DEST_CLICKED";
