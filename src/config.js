8ed1fcff532af81b5c23b1ff017c6aa054a2c53c
Erick Otenyo <otenyo.erick@gmail.com>
2018-09-18 01:40:56 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export const mapboxGLOptions = {
-    mapbox_accessToken: 'pk.eyJ1IjoiY3dob25nbnljIiwiYSI6ImNpczF1MXdrdjA4MXcycXA4ZGtyN2x5YXIifQ.3HGyME8tBs' +
-        '6BnljzUVIt4Q',
+    mapbox_accessToken: 'pk.eyJ1IjoiZXJpY2tvdGVueW8iLCJhIjoiY2owYXlsb2kxMDAwcjJxcDk3a2Q0MmdpZSJ9.GJQzHfNMElZ7OhW_HbnaXw',
     mapStyle: 'mapbox://styles/mapbox/streets-v9',
     center: [
       37.879778, 0.241974 //center the map to kenya
